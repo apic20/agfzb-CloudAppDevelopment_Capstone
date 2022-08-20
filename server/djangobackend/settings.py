b487574b4ec84b328a950c8807559fe36a4a5bc3
Adam Will <apic20@yahoo.com>
2022-08-20 13:11:44 -0400

    Change route

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost","fruitloops.apic.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost","fruitloops.mybluemix.net"]
 
 
 # Application definition
